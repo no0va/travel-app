633b7227c9eab1abe78af6f46de9221436995379
Mohammad Ehsan <m.ehsan1381@gmail.com>
2024-02-27 21:39:21 +0330

    create register page

---
+++ components/user-account/style.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row-reverse",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  title: {
+    color: "#999999",
+    fontSize: 16,
+    fontWeight: "800",
+  },
+  link: {
+    marginRight: 10,
+    color: "#0C359E",
+    fontSize: 16,
+    fontWeight: "800",
+  },
+});
